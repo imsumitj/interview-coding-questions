5d33d180c796ceebf9299adcc8fedd27a1a5e5e3
Sumit J <imsumitj@gmail.com>
2018-07-20 22:53:43 -0700

    Fix toGraphviz method in kotlin code

--- src/main/kotlin/problem/datastructure/trees/bst/BinaryTreeNodes.kt
+++ src/main/kotlin/problem/datastructure/trees/bst/BinaryTreeNodes.kt
@@ -1,17 +1,18 @@
 package problem.datastructure.trees.bst
 
 class BinaryTreeNodes<T>(val data: T) {
+
     var leftNode: BinaryTreeNodes<T>? = null
         set(value) {
             require(value != null) { "LeftNode cannot be set to null" }
             field = value
         }
+
     var rightNode: BinaryTreeNodes<T>? = null
         set(value) {
             require(value != null) { "RightNode cannot be set to null" }
             field = value
         }
-
 
     /**
      * Generates GraphViz diagram
@@ -23,16 +24,14 @@ class BinaryTreeNodes<T>(val data: T) {
         val nodeName = "node" + data.toString()
         val binaryTreeNode = nodeName + "[label = \"<L> |" + data.toString() + "|<R>\"] \n"
 
-        val nodeLeft = if (leftNode != null)
-            "$nodeName:L -> node " + leftNode?.data.toString() + " \n" + leftNode?.toGraphviz()
-        else
-            ""
+        var returnString = binaryTreeNode
 
-        val nodeRight = if ((rightNode != null))
-            ""
-        else
-            "$nodeName:R -> node " + rightNode?.data.toString() + "\n" + rightNode?.toGraphviz()
+        if (leftNode != null)
+            returnString  += "$nodeName:L -> node" + leftNode?.data.toString() + " \n" + leftNode?.toGraphviz()
 
-        return binaryTreeNode + nodeLeft + nodeRight
+        if (rightNode != null)
+            returnString += "$nodeName:R -> node" + rightNode?.data.toString() + "\n" + rightNode?.toGraphviz()
+
+        return returnString
     }
 }
